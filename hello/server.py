9b901ba0a66db37497ea8f7cf490d2363c969899
Kamil Jiwa <kamil.jiwa@gmail.com>
2015-12-14 23:31:39 -0800

    Use the bottle application defined in the module to map routes.

--- hello/server.py
+++ hello/server.py
@@ -5,19 +5,19 @@ from external.bottle import bottle
 application = bottle.default_app()
 
 
-@bottle.route('/')
+@application.route('/')
 def index():
   bottle.response.content_type = 'text/html'
   return bottle.static_file('index.html', 'hello')
 
 
-@bottle.route('/hello.css')
+@application.route('/hello.css')
 def css():
   bottle.response.content_type = 'text/css'
   return bottle.static_file('styles_combined.css', 'hello')
 
 
-@bottle.route('/hello.js')
+@application.route('/hello.js')
 def js():
   bottle.response.content_type = 'application/javascript'
   return bottle.static_file('hello_combined.js', 'hello')
